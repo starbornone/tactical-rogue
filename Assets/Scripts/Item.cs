555a28f71f16d249770b96a259a99fc9eeb3b57d
Sha <sha.kongbrooks@gmail.com>
2024-10-01 23:23:03 +1000

    Moving more files

--- Assets/Scripts/Item.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[System.Serializable]
-public class Item
-{
-    public string id;
-    public string name;
-}
--- Assets/Scripts/ItemsData.cs
+++
@@ -1,7 +0,0 @@
-public static class ItemsData
-{
-    public static Item sword = new Item { id = "sword", name = "Sword" };
-    public static Item shield = new Item { id = "shield", name = "Shield" };
-    public static Item healthPotion = new Item { id = "healthPotion", name = "Health Potion" };
-    public static Item rope = new Item { id = "rope", name = "Rope" };
-}

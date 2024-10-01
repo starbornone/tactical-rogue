a54a868bf592923ce36db330a691c43845c6e64a
Sha <sha.kongbrooks@gmail.com>
2024-10-01 21:41:03 +1000

    Updated TileGenerator script

--- Assets/Scripts/TileGenerator.cs
+++ Assets/Scripts/TileGenerator.cs
@@ -2,34 +2,34 @@ using UnityEngine;
 
 public class TileGenerator
 {
-  private GameObject tilePrefab;
-  private Material topTileMaterial;
-  private Material baseTileMaterial;
+    private GameObject tilePrefab;
+    private Material topTileMaterial;
+    private Material baseTileMaterial;
 
-  public TileGenerator(GameObject tile, Material topMaterial, Material baseMaterial)
-  {
-    tilePrefab = tile;
-    topTileMaterial = topMaterial;
-    baseTileMaterial = baseMaterial;
-  }
-
-  public void GenerateTileColumn(int x, int z, int tileHeight, Transform parent)
-  {
-    for (int y = -12; y <= tileHeight; y++)
+    public TileGenerator(GameObject tile, Material topMaterial, Material baseMaterial)
     {
-      Vector3 position = new Vector3(x, y, z);
-      GameObject tile = Object.Instantiate(tilePrefab, position, Quaternion.identity, parent);
-
-      Renderer tileRenderer = tile.GetComponent<Renderer>();
-
-      if (y == tileHeight)
-      {
-        tileRenderer.material = topTileMaterial;
-      }
-      else
-      {
-        tileRenderer.material = baseTileMaterial;
-      }
+        tilePrefab = tile;
+        topTileMaterial = topMaterial;
+        baseTileMaterial = baseMaterial;
     }
-  }
+
+    public void GenerateTileColumn(int x, int z, int tileHeight, Transform parent)
+    {
+        for (int y = -12; y <= tileHeight; y++)
+        {
+            Vector3 position = new Vector3(x, y, z);
+            GameObject tile = Object.Instantiate(tilePrefab, position, Quaternion.identity, parent);
+
+            Renderer tileRenderer = tile.GetComponent<Renderer>();
+
+            if (y == tileHeight)
+            {
+                tileRenderer.material = topTileMaterial;
+            }
+            else
+            {
+                tileRenderer.material = baseTileMaterial;
+            }
+        }
+    }
 }

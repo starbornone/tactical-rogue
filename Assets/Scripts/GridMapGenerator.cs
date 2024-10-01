43709b004b3acf0fa6035c690f1bfcc59bd30fa8
Sha <sha.kongbrooks@gmail.com>
2024-10-01 21:40:47 +1000

    Updated GridMapGenerator script

--- Assets/Scripts/GridMapGenerator.cs
+++ Assets/Scripts/GridMapGenerator.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System;
 
 public class GridMapGenerator : MonoBehaviour
 {
@@ -6,32 +7,76 @@ public class GridMapGenerator : MonoBehaviour
     public int mapLength = 48;
     public int minTileHeight = 0;
     public int maxTileHeight = 6;
+    public float noiseScale = 20f;
+    public int octaves = 4;
+    [Range(0, 1)]
+    public float persistence = 0.5f;
+    public float lacunarity = 2f;
+    public int seed = 42;
+    public Vector2 offset;
     public GameObject tilePrefab;
     public Material topTileMaterial;
     public Material baseTileMaterial;
 
+    public float[,] heightMap;
+
+    public event Action OnMapGenerated;
+
     private HeightMapGenerator heightMapGenerator;
-    private TileGenerator tileGenerator;
 
     void Start()
     {
         heightMapGenerator = new HeightMapGenerator(mapWidth, mapLength, minTileHeight, maxTileHeight);
-        tileGenerator = new TileGenerator(tilePrefab, topTileMaterial, baseTileMaterial);
+        heightMapGenerator.noiseScale = noiseScale;
+        heightMapGenerator.octaves = octaves;
+        heightMapGenerator.persistence = persistence;
+        heightMapGenerator.lacunarity = lacunarity;
+        heightMapGenerator.seed = seed;
+        heightMapGenerator.offset = offset;
 
         GenerateMap();
     }
 
     void GenerateMap()
     {
-        float[,] heightMap = heightMapGenerator.GenerateHeightMap();
+        heightMap = heightMapGenerator.GenerateHeightMap();
 
         for (int x = 0; x < mapWidth; x++)
         {
             for (int z = 0; z < mapLength; z++)
             {
                 int tileHeight = Mathf.RoundToInt(heightMap[x, z]);
-                tileGenerator.GenerateTileColumn(x, z, tileHeight, transform);
+
+                for (int y = -12; y <= tileHeight; y++)
+                {
+                    Vector3 position = new Vector3(x, y, z);
+                    GameObject tile = Instantiate(tilePrefab, position, Quaternion.identity, transform);
+                    Renderer tileRenderer = tile.GetComponent<Renderer>();
+
+                    if (y == tileHeight)
+                    {
+                        tileRenderer.material = topTileMaterial;
+                    }
+                    else
+                    {
+                        tileRenderer.material = baseTileMaterial;
+                    }
+                }
             }
+        }
+
+        OnMapGenerated?.Invoke();
+    }
+
+    public int GetTileHeight(int x, int z)
+    {
+        if (x >= 0 && x < mapWidth && z >= 0 && z < mapLength)
+        {
+            return Mathf.RoundToInt(heightMap[x, z]);
+        }
+        else
+        {
+            return 0;
         }
     }
 }

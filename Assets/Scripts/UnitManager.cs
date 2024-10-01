d6b92aa982a8f977bac1617752bfcf3b9692d17e
Sha <sha.kongbrooks@gmail.com>
2024-10-01 21:29:31 +1000

    Add UnitManager script

---
+++ Assets/Scripts/UnitManager.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitManager : MonoBehaviour
+{
+    public GameObject unitPrefab;
+
+    void Start()
+    {
+        GridMapGenerator mapGenerator = FindObjectOfType<GridMapGenerator>();
+        mapGenerator.OnMapGenerated += SpawnUnits;
+    }
+
+    void SpawnUnits()
+    {
+        GridMapGenerator mapGenerator = FindObjectOfType<GridMapGenerator>();
+
+        List<UnitData> unitsData = UnitsData.allUnits;
+
+        foreach (UnitData data in unitsData)
+        {
+            int x = data.map.x;
+            int z = data.map.y;
+            int tileHeight = mapGenerator.GetTileHeight(x, z);
+            Vector3 position = new Vector3(x, tileHeight + 1, z);
+            GameObject unitObject = Instantiate(unitPrefab, position, Quaternion.identity);
+            Unit unit = unitObject.GetComponent<Unit>();
+            unit.unitData = data;
+        }
+    }
+}
